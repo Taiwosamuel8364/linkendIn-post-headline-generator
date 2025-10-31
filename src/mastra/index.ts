81f1ad1615a7a3fce449181f8d0d9c42a5e41377
Taiwosamuel8364 <taiwosamuel8364@gmail.com>
2025-10-31 04:45:37 -0700

    updated

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,23 +1,12 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
-import { weatherWorkflow } from "./workflows/weather-workflow";
-import { weatherAgent } from "./agents/weather-agent";
+import { linkedinHeadlineWorkflow } from "./workflows/linkedin-headline.workflow";
 import { linkedinAgent } from "./agents/linkedin-agent";
-import {
-  toolCallAppropriatenessScorer,
-  completenessScorer,
-  translationScorer,
-} from "./scorers/weather-scorer";
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent, linkedinAgent },
-  scorers: {
-    toolCallAppropriatenessScorer,
-    completenessScorer,
-    translationScorer,
-  },
+  workflows: { linkedinHeadlineWorkflow },
+  agents: { linkedinAgent },
   storage: new LibSQLStore({
     // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
